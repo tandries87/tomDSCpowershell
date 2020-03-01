d3eac7f1cf487fb5ee4ec6100a60c6fa53aeb3c5
tandries87 <56696283+tandries87@users.noreply.github.com>
2020-03-01 14:25:40 +0100

    Update ADcreation.ps1
    copy from file used in class

--- ADcreation.ps1
+++ ADcreation.ps1
@@ -1,1 +1,53 @@
+Configuration ADDomain_NewForest_Config
+{
+    param
+    (
+        [Parameter(Mandatory = $true)]
+        [ValidateNotNullOrEmpty()]
+        [System.Management.Automation.PSCredential]
+        $Credential,
+
+        [Parameter(Mandatory = $true)]
+        [ValidateNotNullOrEmpty()]
+        [System.Management.Automation.PSCredential]
+        $SafeModePassword
+    )
+
+    Import-DscResource -ModuleName PSDesiredStateConfiguration
+    Import-DscResource -ModuleName ActiveDirectoryDsc
+
+    node $allnodes.nodename
+    {
+        WindowsFeature 'ADDS'
+        {
+            Name   = 'AD-Domain-Services'
+            Ensure = 'Present'
+        }
+
+        WindowsFeature 'RSAT'
+        {
+            Name   = 'RSAT-AD-PowerShell'
+            Ensure = 'Present'
+        }
+
+        ADDomain 'shutterup.local'
+        {
+            DomainName                    = 'shutterup.local'
+            Credential                    = $Credential
+            SafemodeAdministratorPassword = $SafeModePassword
+            ForestMode                    = 'WinThreshold'
+        }
+    }
+}
+$cd = @{
+    AllNodes = @(    
+        @{  
+            NodeName = "localhost"
+            PsDscAllowPlainTextPassword = $true
+        }
+    ) 
+}
+ADDomain_NewForest_Config -ConfigurationData $cd
+
+
 
